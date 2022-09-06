0b427bbf7da2437e2779f192ea957f684b1f7984
Kamil Choiński <110kc3@gmail.com>
2022-09-06 19:30:25 +0200

    Changing definition

--- locustfile.py
+++ locustfile.py
@@ -1,28 +1,29 @@
 
-from locust import HttpUser, TaskSet,task,between
+import time
+from locust import HttpUser, User,task,between
 
-class UserBehaviour(TaskSet):
 
-    @task
-    def bubblesort_post(self):
-      response= self.client.post("/api/bubblesort",
-      data={
-        "numbers_to_sort": [2154, 3845, 4141, 8050, 1106, 4285, 1067, 7663, 823, 7363, 8443, 434, 5016, 3, 55],
-        "save_in_db": 0
-        })
-      print(response.text)
-      print(response.headers)
-      print(response.status_code)
-      print(response.request.headers)
 
-      json_var = response.json()
-      number_of_elements = json_var['number_of_elements']
-      print( json_var + ' Post title is ' + number_of_elements)
+
+def bubblesort_post(self):
+  response= self.client.post("/api/bubblesort",
+  data={
+    "numbers_to_sort": [2154, 3845, 4141, 8050, 1106, 4285, 1067, 7663, 823, 7363, 8443, 434, 5016, 3, 55],
+    "save_in_db": 0
+    })
+  print(response.text)
+  print(response.headers)
+  print(response.status_code)
+  print(response.request.headers)
+
+  json_var = response.json()
+  number_of_elements = json_var['number_of_elements']
+  print( json_var + ' Post title is ' + number_of_elements)
 
 
 
 class User(HttpUser):
-    task_set=[UserBehaviour]
+    task_set=[bubblesort_post]
     wait_time = between(5, 10)
     host="http://34.118.0.152:8080"
 
